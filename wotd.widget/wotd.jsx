92f02c9471419c273130318343d87b26237ac7c9
NVRC <nathaniel.charlebois@gmail.com>
2019-03-06 15:03:13 -0500

    Simple usage implemented but response.status=429 not properly handled

--- wotd.widget/wotd.jsx
+++ wotd.widget/wotd.jsx
@@ -6,17 +6,14 @@ const config = require( "./src/config.json" )
 export const get_current_date = () => {
     var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth() + 1; //January is 0!
+    var mm = today.getMonth() + 1; //January indexed-0
     var yyyy = today.getFullYear();
-
     if (dd < 10) {
       dd = '0' + dd;
     }
-
     if (mm < 10) {
       mm = '0' + mm;
     }
-
     today = yyyy+'-'+mm+'-'+dd;
     return today;
 }
@@ -67,19 +64,26 @@ const PartOfSpeech = styled("h2")`
 
 export const initialState = { output: 'Fetching data' };
 
-/*
+
 export const command = (dispatch) =>
   fetch(wotd_url)
     .then((response) => {
-        response.json().then((data)=>{
-            //data = { } from wordnik api
-            dispatch({ type: 'FETCH_SUCCEDED', output: data  });
-        })
+        if(!response.ok){
+            console.log(response.status);
+            const status = response.status;
+            dispatch({ type: 'FETCH_FAILED', error: status });
+        } else {
+            response.json().then((data)=>{
+                //data = { } from wordnik api
+                dispatch({ type: 'FETCH_SUCCEDED', output: data  });
+            })
+        }
     })
     .catch((error) => {
       dispatch({ type: 'FETCH_FAILED', error: error });
     });
-*/
+
+
 
 export const updateState = (event, previousState) => {
   if (event.error) {
@@ -93,18 +97,19 @@ export const updateState = (event, previousState) => {
 
 
 
-export const refreshFrequency = false; // ms
+export const refreshFrequency = 86400; // a full 24 hours (ms)
 
 export const render = ({ output, error }) => {
+
     return error ? (
         <Container>
-            <div>Error: <strong>{output}</strong></div>
+            <div>Error: <strong>{error}</strong></div>
         </Container>
     ) : (
         <Container>
-            <Word>{config.test_data.word}</Word>
-            <Note>{config.test_data.note}</Note>
-            {config.test_data.definitions.map((def, idx) => (
+            <Word>{output.word}</Word>
+            <Note>{output.note}</Note>
+            {output.definitions.map((def, idx) => (
 
                 renderDef(def, idx)
             ))}
